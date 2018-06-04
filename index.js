36fa930a0ce4bc782478bad38c09154b0e0fb133
shane <tjdghks0531@gmail.com>
2018-06-04 19:19:56 +0900

    set influxDB

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const cron = require('node-cron')
 const influx = require('influx')
 
+const DB = new influx.InfluxDB({
+  host: 'localhost',
+  port: 8086,
+  protocol: 'http',
+  username: 'id',
+  password: 'password',
+  database: 'grafana'
+})
+
 cron.schedule('*/1 * * * *', function(){
   console.log('1분마다 실행되는 작업');
 });
